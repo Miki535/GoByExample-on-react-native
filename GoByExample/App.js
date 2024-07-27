23cbee7e0bfc43228f6d04345a3481c7daf91629
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-07-27 08:56:16 +0000

    Update

---
+++ GoByExample/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.main}></Text>
+    <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1f1f1f',
+    alignItems: 'center',
+    
+  },
+  text: {
+    color: '#dadada'
+  },
+  main: {
+    paddingTop: 60,
+    height: 100,
+    color: 'white',
+  }
+});

56ca9d78b48b0b828e22ad7ff1074c592e360703
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-07-27 09:34:18 +0000

    Update App.js

--- GoByExample/App.js
+++ GoByExample/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-
       <Text style={styles.main}>Go by Example</Text>
+      <ScrollView>
+      <Text></Text>
+      <Text style={styles.text}>Go is an open source programming language designed for building scalable, secure and reliable software. Please read the official documentation to learn more. </Text>
+      <Text></Text>
+      <Text style={styles.text}>Go by Example is a hands-on introduction to Go using annotated example programs. Check out the first example or browse the full list below. </Text>
+      <Text></Text>
+      <Text style={styles.text}>Unless stated otherwise, examples here assume the latest major release Go. </Text>
+     </ScrollView>
     <StatusBar style="auto" />
     </View>
   );
@@ -19,7 +26,9 @@ const styles = StyleSheet.create({
     
   },
   text: {
-    color: '#dadada'
+    color: '#dadada',
+    fontSize: '13.5',
+    fontWeight: '200',
   },
   main: {
     paddingTop: 60,

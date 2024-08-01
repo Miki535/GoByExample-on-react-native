67b545a34fd7c6357f0d52c6022768782d15a189
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-08-01 14:32:46 +0000

    Create ValuesScreen.js

---
+++ GoByExample/screens/ValuesScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ValuesScreen = () => {
+
+    return (
+        <View>
+            <Text>Values Screen Test</Text>
+        </View>
+    )
+
+}
+
+export default ValuesScreen;
